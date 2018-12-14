faefb8b91cd9c16977e30481ef363a19358c05bb
Bence Szilveszter Hajdu <benc0054@easv365.dk>
2018-12-14 09:16:19 +0100

    Made it easier to read

--- SumOfSquaresOneToN/Program.cs
+++ SumOfSquaresOneToN/Program.cs
@@ -12,11 +12,13 @@ namespace SumOfSquaresOneToN
         {
             Start:
             //Intro to what the program does
-            Console.WriteLine("Please enter a natural number and press Enter! The program will calculate the sum of the squares of all natural numbers from one to your number.");
+            Console.WriteLine("Please enter a natural number and press Enter! " +
+                "The program will calculate the sum of the squares of all natural numbers from one to your number.");
             //User input
             long limit = Convert.ToInt32(Console.ReadLine());
             //Further instructions
-            Console.WriteLine("Please enter how many times you want to repeat the calculation (in order to get an accurate average processing time).");
+            Console.WriteLine("Please enter how many times you want to repeat the calculation " +
+                "(in order to get an accurate average processing time).");
             //User input
             int repeat = Convert.ToInt32(Console.ReadLine());
 
@@ -37,9 +39,8 @@ namespace SumOfSquaresOneToN
                 watch.Stop();
                 times.Add(watch.ElapsedMilliseconds);
             }
-            //Write results with average execution time
+            //Write results with average execution time and go back to Start
             Console.WriteLine("The result is: " + sum + ", and it took " + times.Average() + " milliseconds to calculate.");
-
             goto Start;
         }
     }
